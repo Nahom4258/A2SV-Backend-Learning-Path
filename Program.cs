a6e79dc7b4c8eea19045e8b4eb3dd5bdc86a4ec5
Nahom Habtamu <nhabtamu42@gmail.com>
2023-08-01 15:28:55 +0300

    project files

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿System.Console.Write("Enter a string: ");
+string input = Console.ReadLine();
+
+int left_ptr = 0, right_ptr = input.Length-1;
+
+bool isPalindrome = true;
+
+while (left_ptr < right_ptr)
+{
+    if (input[left_ptr] != input[right_ptr])
+    {
+        isPalindrome = false;
+        break;
+    }
+
+    left_ptr += 1;
+    right_ptr -= 1;
+}
+
+System.Console.WriteLine(isPalindrome ? $"{input} is PALINDROME!" : $"{input} is NOT Palindrome!!");
