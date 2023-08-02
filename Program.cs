07f85149e7819c874ca0d66b42e583cb179a4bf3
Nahom Habtamu <nhabtamu42@gmail.com>
2023-08-02 16:02:36 +0300

    add task 2 program execution

--- Program.cs
+++ Program.cs
@@ -8,3 +8,17 @@ circle.PrintShapeArea();
 rectangle.PrintShapeArea();
 square.PrintShapeArea();
 triangle.PrintShapeArea();
+
+// Day 2 - Task 2 execution
+Library abrehot = new Library("Abrehot", "4 Kilo");
+// add books
+abrehot.AddBook(new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", 1925));
+abrehot.AddBook(new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084", 1960));
+abrehot.AddBook(new Book("1984", "George Orwell", "9780451524935", 1949));
+
+// add media items
+abrehot.AddMediaItem(new MediaItem("Inception", "DVD", 154));
+abrehot.AddMediaItem(new MediaItem("Thriller", "CD", 42));
+abrehot.AddMediaItem(new MediaItem("Planet Earth II", "DVD", 300));
+
+abrehot.PrintCatalog();
