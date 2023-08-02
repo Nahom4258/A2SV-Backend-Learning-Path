07c5454dc7388b810b4f38c44a7c14ba5f1030a3
Nahom Habtamu <nhabtamu42@gmail.com>
2023-08-02 13:16:54 +0300

    main method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Day 2 - Task 1 execution
+Circle circle = new Circle("Circle", 35);
+Rectangle rectangle = new Rectangle("Rectangle", 4, 7);
+Rectangle square = new Rectangle("Sqare", 5);
+Triangle triangle = new Triangle("Triangle", 8, 2);
+
+circle.PrintShapeArea();
+rectangle.PrintShapeArea();
+square.PrintShapeArea();
+triangle.PrintShapeArea();
